ccdaa2a875bbdd91a8b9615bf70b057494945ec3
KALURIBHARATHI <kaluribharathi26@gmail.com>
2025-05-09 09:27:08 +0530

    Add files via upload

---
+++ assignment.sql
@@ -0,0 +1,36 @@
+create table employee
+(
+ID int primary key,
+first_name varchar (50),
+last_name varchar (20),
+email varchar (50),
+department varchar (100)
+)
+
+insert into employee values ('101','cherith','reddy','cherithreddy23@gmail.com','hr')
+
+insert into employee values ('102','robed','wood','robedwood12@gmail.com','finan')
+
+insert into employee values ('103','sia','cheap','siacheap14@gmail.com','sale')
+
+insert into employee values ('104','ellie','gould','elliegould16@gmail.com','hr')
+
+insert into employee values ('105','john','deo','johndeo18@gmail.com','hr')
+insert into employee values ('106','sri','bhavya','sri bhavya24@gmail.com','finan')
+insert into employee values ('107','bhavan','sri','BHAVANSRI25@gmail.com','sale')
+insert into employee values ('108','jinka','sowghya','jinkasowghya29@gmail.com','health')
+insert into employee values ('109','kaluri','bhargavi','kaluribhargavi13@gmail.com','sale')
+insert into employee values ('110','vara','lakshmi','varalakshmi56@gmail.com','health')
+insert into employee values ('111','mahesh','reddy','mahesh 5654@gmail.com','IT')
+insert into employee values ('112','venkata','madhu','madhu 5457@gmail.com','IT')
+insert into employee values ('113','rajoli','reddy','rajoli reddy21@gmail.com','SALE')
+SELECT*FROM EMPLOYEE
+select concat ('cherith','','reddy') as fullname
+
+
+
+
+
+
+
+
